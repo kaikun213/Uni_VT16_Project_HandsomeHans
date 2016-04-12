5de3dd80b11b7aa74e6f7676d7c38b578d581093
kaikun213 <kaktus.w.h@gmx.de>
2016-04-12 15:19:12 +0200

    resource/css folder for mainpage design

--- SmugglerShop/src/pages/Mainpage_jakob.java
+++ SmugglerShop/src/pages/Mainpage_jakob.java
@@ -5,11 +5,14 @@ package pages;
 
 import java.io.Serializable;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import baseClasses.Page;
+import baseClasses.Product;
 
 /**
  * @author kaikun
@@ -30,7 +33,34 @@ public class Mainpage_jakob extends Page implements Serializable {
 	}
 	
 	public ResultSet getContent(){
+		setContent("SELECT * FROM webshopDB.product;");
 		return content;
+	}
+	
+	public List<Product> getProducts(){
+		List<Product> arr = new ArrayList<Product>();
+		Product test = new Product();
+		test.setProductName("test");
+		test.setDescription("Describe the product");
+		arr.add(test);
+		
+		Product test1 = new Product();
+		test1.setProductName("test");
+		test1.setDescription("Describe the product");
+		arr.add(test1);
+		
+		
+		Product test2 = new Product();
+		test2.setProductName("test");
+		test2.setDescription("Describe the product");
+		arr.add(test2);
+		
+		Product test3 = new Product();
+		test3.setProductName("test");
+		test3.setDescription("Describe the product");
+		arr.add(test3);
+		
+		return arr;
 	}
 
 }
