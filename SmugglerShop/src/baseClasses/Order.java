9e88ab0cf5d9b9156a8263636d7c4e309859e88d
paulius1994zukas <paulius1994zukas@gmail.com>
2016-04-15 15:38:56 +0200

    Updated Order class.
    Added some extra methods, fixed problems from previous version and added javaDoc comments.

--- SmugglerShop/src/baseClasses/Order.java
+++ SmugglerShop/src/baseClasses/Order.java
@@ -1,10 +1,9 @@
-/**
- * 
- */
-package baseClasses;
+package mano_uzduotys_mokantis;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 /**
  * This class represents an order.
@@ -14,60 +13,155 @@ import java.util.Date;
  */
 public class Order {
 
+    // Fields
+    private enum OrderStatus{IN_PROCESS, SHIPPED, DELIVERED}
+    private OrderStatus orderStatus;
     private long orderId;
-    private List<Product> orderList;
-    public enum orderStatus {New,Shippable,Delivered};
-    private orderStatus status;
+    private List<Product> orderList = new List<Product>();
     private Date orderDate;
+    private int size;
 
+    /**
+     *  Empty constructor.
+     */
     public Order() {
     }
 
-    public Order(long orderId, List<Product> orderList, orderStatus status, Date orderDate) {
+    /**
+     *  Order constructor.
+     *
+     *  @param orderId
+     *  @param orderList
+     *  @param orderDate
+     *  @param orderStatus
+     */
+    public Order(long orderId, List<Product> orderList, Date orderDate, OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
         this.orderId = orderId;
         this.orderList = orderList;
-        this.status = status;
         this.orderDate = orderDate;
     }
 
-    public long getOrderId() {
-        return orderId;
-    }
+    // Getters and setters for fields below.
 
-    public void setOrderId(long orderId) {
+    /**
+     *  Method returning order status.
+     *  @return OrderStatus
+     */
+    public OrderStatus getOrderStatus() { return orderStatus; }
+
+    /**
+     *  Method to set the order status.
+     *  @param orderStatus
+     */
+    public void setOrderStatus(OrderStatus orderStatus) { this.orderStatus = orderStatus; }
+
+    /**
+     *  Method returning order id.
+     *  @return long
+     */
+    public long getOrderId() { return orderId; }
+
+    /**
+     * Method setting order id.
+     * @param orderId
+     */
+    public void setOrderId(int orderId) {
         this.orderId = orderId;
     }
 
+    /**
+     * Method returning list of products.
+     * @return List<Product>
+     */
     public List<Product> getOrderList() {
         return orderList;
     }
 
-    public void setOrderList(List<Product> orderList) {
-        this.orderList = orderList;
-    }
+    /**
+     *  Method to set the list of products.
+     *  @param orderList
+     */
+    public void setOrderList(List<Product> orderList) { this.orderList = orderList; }
 
-    public orderStatus getOrderStatus() {
-        return status;
-    }
-
-    public void setOrderStatus(orderStatus orderStatus) {
-        this.status = orderStatus;
-    }
-
+    /**
+     *  Method returning the order date.
+     *  @return Date
+     */
     public Date getOrderDate() {
         return orderDate;
     }
 
+    /**
+     *  Method to set the date of the order.
+     *  @param orderDate
+     */
     public void setOrderDate(Date orderDate) {
         this.orderDate = orderDate;
     }
 
+    /**
+     *  Method to get the product by its position in the list.
+     *  @param n
+     *  @return Product
+     */
+    public Product getItem(int n) { return orderList.get(n); }
+
+    /**
+     *  Method returning the string output of the order.
+     *  @return String
+     */
     @Override
     public String toString() {
-        return "Order{" +
-                "id = " + orderId +
-                ", status = '" + status + '\'' +
-                ", orderDate = " + orderDate +
-                '}';
+        return orderId + " " + " " + orderDate + " " + "\n" + orderList;
+    }
+
+    /**
+     *  Class that represents order iterator.
+     */
+    public class OrderIterator implements Iterator<Product> {
+
+        private final List<Product> orderList;
+        private int index;
+
+        /**
+         *  Order iterator.
+         *  @param orderList
+         */
+        public OrderIterator(List<Product> orderList) {
+            this.orderList = orderList;
+            index = 0;
+        }
+
+        /**
+         *  Method to return next product in the list.
+         *  @return Product
+         */
+        @Override
+        public Product next() {
+            if(hasNext())
+                return orderList.get(index++);
+            else
+                throw new NoSuchElementException("There are no products!");
+        }
+
+        /**
+         *  Method to check whether there is next product in the list.
+         *  @return boolean (true/false)
+         */
+        @Override
+        public boolean hasNext() {
+            return !(orderList.size() != index);
+        }
+
+        /**
+         *  Method to remove a product from the list.
+         */
+        @Override
+        public void remove() {
+            if(index <= 0)
+                throw new IllegalStateException("You can't delete element before first next() method call");
+            orderList.remove(--index);
+        }
     }
 }
