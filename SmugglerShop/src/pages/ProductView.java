36b91bf77ba22f023878b34bcf505f1ff801bc4f
Benjamin <benjamin.svard@gmail.com>
2016-04-22 18:05:30 +0200

    Modified ProductView class to make it more uniform to rest of program, added comments

--- SmugglerShop/src/pages/ProductView.java
+++ SmugglerShop/src/pages/ProductView.java
@@ -2,29 +2,53 @@ package pages;
 
 import javax.inject.Named;
 
-import baseClasses.ConnectionClass;
 import baseClasses.Product;
+import baseClasses.Page;
 
 import java.io.Serializable;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.enterprise.context.Dependent;
 import javax.enterprise.context.SessionScoped;
+/**
+ * Henry and Ben 22-04-16
+ */
 
 @Named("productView")
 @SessionScoped
-public class ProductView implements Serializable {
+public class ProductView extends Page implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	// Array with hopefully 1 product
+	private List<Product> products = new ArrayList<Product>();
 	
-	public Product magic(int id){
-		String command = "SELECT * FROM webshopDB.product WHERE id="+id;
-		ConnectionClass conn = new ConnectionClass();
-		ResultSet rs = conn.fetch(command);
-		
-		return new Product(rs);
+	/*
+	 * Command line to fetch the product
+	 * "setContent" and "content" are from page class
+	 */
+	public void setProduct(int id) {
+		try {
+			setContent("SELECT * FROM webshopDB.product WHERE id="+id);
+			products = toProducts(content);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	/*
+	 * Get int value ID from xhtml and fetch the product 
+	 */
+	public Product getOneProduct(int id){
+		setProduct(id);
+		
+		//Dummy class if enter illegal ID
+		if(products.size()==0){
+			Product prod = new Product();			
+			return prod;
+		}
+		return products.get(0);
+	}	
+	
 }
