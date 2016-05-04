2d4e39566f6edbae6270da71c6644c609350ea78
kaikun213 <kaktus.w.h@gmx.de>
2016-05-04 10:30:47 +0200

    Admin bean page for all functions

---
+++ SmugglerShop/src/pages/AdminPages.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package pages;
+
+import java.io.Serializable;
+
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import baseClasses.Page;
+
+/**
+ * @author kaikun
+ *
+ */
+
+@Named
+@SessionScoped
+public class AdminPages extends Page implements Serializable {
+	
+	/**
+	 * Default serialVersionID generated from eclipse
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+}
